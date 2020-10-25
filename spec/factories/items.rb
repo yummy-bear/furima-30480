7e34f432dd1882142364227ba24b28c2c57c19ed
yummy-bear <mudskipper7ki@gmail.com>
2020-10-25 20:53:44 +0900

    テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    introduction {Faker::Lorem.sentence}
+    category_id {"2"}
+    status_id {"2"}
+    delivery_day_id {"2"}
+    delivery_area_id {"2"}
+    delivery_fee_id {"2"}
+    price {"333"}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
