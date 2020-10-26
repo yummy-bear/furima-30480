992fb070ed03375a2f7caa37531227e68d3678d1
yummy-bear <mudskipper7ki@gmail.com>
2020-10-26 16:36:59 +0900

    商品削除機能の実装改

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if @item.destroy
+      redirect_to root_path 
+    else
+      render :show
+    end
   end
 
   private
